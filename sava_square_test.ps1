3ff4c85be78b624dd75171b92ba3c641c4306c90
cmshaw95 <colinmshaw95@gmail.com>
2021-06-29 19:25:10 -0400

    Delete sava_square_test.ps1

--- sava_square_test.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-$getPayments = Invoke-WebRequest -Uri https://connect.squareup.com/v2/payments -Headers @{'Square-Version'='2020-04-22'; 'Authorization' = 'Bearer EAAAETiZWYursLwLLN-X1Y7PsDCMaG_dWmK1Ivk_B8enyhuv_XA3gKJUQZQL_l7h'; 'Content-Type'='application/json'}
-#$payment_id = $getPayments
-$getPaymentID = Invoke-WebRequest -Uri https://connect.squareup.com/v2/payments/zRMi7t2y2hAhpzdfXVvkiu8DvaB -Headers @{'Square-Version'='2020-04-22'; 'Authorization' = 'Bearer EAAAETiZWYursLwLLN-X1Y7PsDCMaG_dWmK1Ivk_B8enyhuv_XA3gKJUQZQL_l7h'; 'Content-Type'='application/json'}
-$getCustomers = Invoke-WebRequest -Uri https://connect.squareup.com/v2/customers -Headers @{'Square-Version'='2020-04-22'; 'Authorization' = 'Bearer EAAAETiZWYursLwLLN-X1Y7PsDCMaG_dWmK1Ivk_B8enyhuv_XA3gKJUQZQL_l7h'; 'Content-Type'='application/json'}
-$getLocation = Invoke-WebRequest -Uri https://connect.squareup.com/v2/locations -Headers @{'Square-Version'='2020-04-22'; 'Authorization' = 'Bearer EAAAETiZWYursLwLLN-X1Y7PsDCMaG_dWmK1Ivk_B8enyhuv_XA3gKJUQZQL_l7h'; 'Content-Type'='application/json'}
-
-$getPayments.Content | ConvertFrom-Json | ConvertTo-Json
